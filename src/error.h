c8dd6ee712f063a78adbbb12c16de7c0fcbbafa3
Fergons <fergons777@gmail.com>
2020-11-05 20:31:29 +0100

    upravy

--- src/error.h
+++ src/error.h
@@ -5,7 +5,7 @@
 #include <stdio.h>
 
 typedef enum error{
-  ERROR_SUCC = 0,  //Success
+  SUCCESS = 0,  //Success
   ERROR_LEX = 1,  //chyba v rámci lexikálnej analýzy
   ERROR_SYN = 2,  //chyba v rámci syntaktickej analýzy
   ERROR_SEM_DEF = 3,  //sémantická chyba v programe - nedefinovaná funkcia/premenná
@@ -14,5 +14,6 @@ typedef enum error{
   ERROR_SEM_PAR = 6,  //sémantická chyba v programe - zlý počet/typ parametrov či návratových hodnôť u volania/návratu funkcie
   ERROR_SEM_OTHER = 7,  //ostatné sémantické chyby
   ERROR_NULL = 9,  //sémantická chyba delenia nulovou konštantou
+  ERROR_STR  = 10, //chyba pri praci s retazcami
   ERROR_TRANS = 99,  //interná chyba prekladača (napr chyba alokácie, pamäti ...)
 } ERROR;
