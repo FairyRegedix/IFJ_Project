aca1223ff79206e2f86cc91e6bf586e28f0f87a6
Megonasayit <hrmorichard2@gmail.com>
2020-11-27 00:23:28 +0100

    adding new functions

--- src/generator.c
+++ src/generator.c
@@ -4,7 +4,6 @@
 
 #include "generator.h"
 
-//finished functions
 void generate_header(){
    
     printf(".IFJcode20\n");
@@ -65,21 +64,21 @@ void gen_call(char* function){
 }
 
 void gen_return(){
-    printf("RETURN");
+    printf("RETURN\n");
 }
 
 void gen_WRITE(char* s1){
-    printf("WRITE LF%s", s1);
+    printf("WRITE LF%s\n", s1);
 }
 
 void gen_LABEL_start(char* label){
-    printf("LABEL $%s", label);
-    printf("PUSHFRAME");
+    printf("LABEL $%s\n", label);
+    printf("PUSHFRAME\n");
 }
 
 void gen_LABEL_end(){
-    printf("POPFRAME");
-    printf("RETURN");
+    printf("POPFRAME\n");
+    printf("RETURN\n");
 }
 
 void gen_start_of_function(char* function){
@@ -89,11 +88,11 @@ void gen_start_of_function(char* function){
 
 void gen_end_of_function(){
     printf("#End of function\n");
-    printf("\n");
     gen_LABEL_end();
 }
 
 void gen_if_start(char* label, int id){
+    printf("#IF $%s$if$%i\n",label, id);
     printf("JUMPIFNEQ $%s$if$%i$else\n", label, id);
 }
 
@@ -107,6 +106,7 @@ void gen_if_end(char* label, int id){
 }
 
 void gen_while_start(char* label, int id){
+    printf("#WHILE $%s$while$%i\n",label, id);
     printf("LABEL $%s$while$%i$start\n", label, id);
 }
 
@@ -115,135 +115,194 @@ void gen_while_end(char* label, int id){
     printf("LABEL $%s$while$%i$end\n", label, id);
 }
 
-void gen_JUMP(char* destination){
-    printf("JUMP $%s", destination);
+void gen_for_start(char* label, int id, char* LTS_or_GTS, char* s1, char* s2, bool equals){
+    printf("#FOR $%s$for$%i\n",label, id);
+    if(equals){
+        if(str_cmp(LTS_or_GTS, "LTS")){
+        printf("SUB %s int@1\n", s1);
+    }
+    else{
+        printf("ADD %s int@1\n", s1);
+    }
+    }    
+    printf("LABEL $%s$for$%i$start\n", label, id);
+    printf("PUSHS %s\n", s2);
+    printf("PUSHS %s\n", s1);
+    printf("%s\n", LTS_or_GTS);
+    printf("PUSHS bool@true\n");
+    printf("JUMPIFNEQS $%s$for$%i$end\n");
+    if(str_cmp(LTS_or_GTS, "LTS")){
+        printf("ADD %s int@1\n", s1);
+    }
+    else{
+        printf("SUB %s int@1\n", s1);
+    }
 }
 
-//end of finished functions
+void gen_for_end(char* label, int id){
+    printf("JUMP $%s$for$%i$start\n", label, id);
+    printf("LABEL $%s$for$%i$end\n", label, id);  
+}
+
+void gen_JUMP(char* destination){
+    printf("JUMP $%s\n", destination);
+}
+
+void gen_JUMPIFEQ(char* destination, char* s1, char *s2){
+    printf("JUMPIFEQ $%s %s %s\n", destination, s1, s2);
+}
+
+void gen_JUMPIFNEQ(char* destination, char* s1, char *s2){
+    printf("JUMPIFEQ $%s %s %s\n", destination, s1, s2);
+}
+
+void gen_stack_instructions(stack_instruction instruction){
+    switch(instruction){
+        case ADDS:
+            printf("ADDS\n");
+            break;
+        case SUBS:
+            printf("SUBS\n");
+            break;
+        case MULS:
+            printf("MULS\n");
+            break;
+        case DIVS:
+            printf("DIVS\n");
+            break;
+        case IDIVS:
+            printf("IDIVS\n");
+            break;
+        case LTS:
+            printf("LTS\n");
+            break;
+        case GTS:
+            printf("GTS\n");
+            break;
+        case EQS:
+            printf("EQS\n");
+            break;
+        case ANDS:
+            printf("ANDS\n");
+            break;
+        case ORS:
+            printf("ORS\n");
+            break;
+        case NOTS:
+            printf("NOTS\n");
+        default:
+            break;
+        
+    }
+}
 
 void gen_pushs(char* s1){
     printf("PUSHS %s\n", s1);
 }
 
 void gen_pops(char* s1){
-    printf("POPS %s", s1);
+    printf("POPS %s\n", s1);
 }
 
 void gen_clears(){
-    printf("CLEARS");
-}
-
-void_gen_adds(){
-
+    printf("CLEARS\n");
 }
 
 void gen_add(char* s1, char* s2, char* s3){
-    printf("ADD TF@%s @%s %s", s1, s2, s3);
+    printf("ADD TF@%s @%s %s\n", s1, s2, s3);
 }
 
 void gen_sub(char* s1, char* s2, char* s3){
-    printf("SUB TF@%s @%s %s", s1, s2, s3);
+    printf("SUB TF@%s @%s %s\n", s1, s2, s3);
 }
 
 void gen_mul(char* s1, char* s2, char* s3){
-    printf("MUL TF@%s @%s %s", s1, s2, s3);
+    printf("MUL TF@%s @%s %s\n", s1, s2, s3);
 }
 
 void gen_div(char* s1, char* s2, char* s3){
-    printf("DIV TF@%s @%s %s", s1, s2, s3);
+    printf("DIV TF@%s @%s %s\n", s1, s2, s3);
 }
 
 void gen_idiv(char* s1, char* s2, char* s3){
-    printf("IDIV TF@%s @%s %s", s1, s2, s3);
+    printf("IDIV TF@%s @%s %s\n", s1, s2, s3);
 }
 
-
 void gen_LT(char* s1, char* s2, char* s3){
-    printf("LT %s %s %s", s1, s2, s3);
+    printf("LT %s %s %s\n", s1, s2, s3);
 }
 
 void gen_GT(char* s1, char* s2, char* s3){
-    printf("GT %s %s %s", s1, s2, s3);
+    printf("GT %s %s %s\n", s1, s2, s3);
 }
 
 void gen_EQ(char* s1, char* s2, char* s3){
-    printf("EQ %s %s %s", s1, s2, s3);
+    printf("EQ %s %s %s\n", s1, s2, s3);
 }
 
-//miesto pre zasobnikove verzie LTS/GTS/EQS
-
 void gen_AND(char* s1, char* s2, char* s3){
-    printf("AND %s %s %s", s1, s2, s3);
+    printf("AND %s %s %s\n", s1, s2, s3);
 }
 
 void gen_OR(char* s1, char* s2, char* s3){
-    printf("OR %s %s %s", s1, s2, s3);
+    printf("OR %s %s %s\n", s1, s2, s3);
 }
 
 void gen_NOT(char* s1, char* s2, char* s3){
-    printf("NOT %s %s %s", s1, s2, s3);
+    printf("NOT %s %s %s\n", s1, s2, s3);
 }
 
 void gen_INT2FLOAT(char* s1, char* s2){
-    printf("INT2FLOAT %s %s", s1, s2);
+    printf("INT2FLOAT %s %s\n", s1, s2);
 }
 
 void gen_FLOAT2INT(char* s1, char* s2){
-    printf("FLOAT2INT %s %s", s1, s2);
+    printf("FLOAT2INT %s %s\n", s1, s2);
 }
 
 void gen_INT2CHAR(char* s1, char* s2){
-    printf("INT2CHAR %s %s", s1, s2);
+    printf("INT2CHAR %s %s\n", s1, s2);
 }
 
 void gen_STRI2INT(char* s1, char* s2){
-    printf("STRI2INT %s %s", s1, s2);
+    printf("STRI2INT %s %s\n", s1, s2);
 }
-
-//miesto pre zasobnikove verzie INT2FLOATS/FLOAT2INTS/INT2CHARS/STRI2INTS
 
 void gen_READ(char* s1, char* s2){
-    printf("READ %s %s", s1, s2);
+    printf("READ %s %s\n", s1, s2);
 }
 
-
-
 void gen_CONCAT(char* s1, char* s2, char* s3){
-    printf("CONCAT %s %s %s", s1, s2, s3);
+    printf("CONCAT %s %s %s\n", s1, s2, s3);
 }
 
 void gen_STRLEN(char* s1, char* s2){
-    printf("STRLEN %s %s", s1, s2);
+    printf("STRLEN %s %s\n", s1, s2);
 }
 
 void gen_GETCHAR(char* s1, char* s2, char* s3){
-    printf("GETCHAR %s %s %s", s1, s2, s3);
+    printf("GETCHAR %s %s %s\n", s1, s2, s3);
 }
 
 void gen_SETCHAR(char* s1, char* s2, char* s3){
-    printf("SETCHAR %s %s %s", s1, s2, s3);
+    printf("SETCHAR %s %s %s\n", s1, s2, s3);
 }
 
 void gen_TYPE(char* s1, char* s2){
-    printf("TYPE %s %s", s1, s2);
+    printf("TYPE %s %s\n", s1, s2);
 }
 
-
-
-
-
 void gen_EXIT(char* s1){
-    printf("EXIT %s", s1);
+    printf("EXIT %s\n", s1);
 }
 
 void gen_BREAK(){
-    printf("BREAK");
+    printf("BREAK\n");
 }
 
 void gen_DPRINT(char* s1){
-    printf("DPRINT %s", s1);
+    printf("DPRINT %s\n", s1);
 }
-
 
 void gen_EOL(){
     printf("\n");
--- src/generator.h
+++ src/generator.h
@@ -2,22 +2,34 @@
 #define GENERATOR_H
 
 
-
 #include "libraries.h"
 #include "error.h"
 #include "symtable.h"
 #include "scanner.h"
 #include "str.h"
 
+typedef enum{
+    ADDS = 0,
+    SUBS = 1,
+    MULS = 2,
+    DIVS = 3,
+    IDIVS = 4,
+    LTS = 5,
+    GTS = 6,
+    EQS = 7,
+    ANDS = 8,
+    ORS = 9,
+    NOTS = 10,
+}stack_instruction;
+
+void generate_header();
+void generate_start_of_main();
+void generate_end_of_main();
 void gen_defvar(char* id_of_variable);
 void gen_retval(data_type type);
 void gen_move_to_defvar(char* id_of_variable, data_type type);
 char* gen_var_value(data_type type);
-
 void codeGenerator();
-void generate_header();
-void generate_start_of_main();
-void generate_end_of_main();
 void gen_call(char* function);
 void gen_return();
 void gen_WRITE(char* s1);
@@ -30,9 +42,12 @@ void gen_if_else(char* label, int id);
 void gen_if_end(char* label, int id);
 void gen_while_start(char* label, int id);
 void gen_while_end(char* label, int id);
+void gen_for_start(char* label, int id, char* LTS_or_GTS, char* s1, char* s2, bool equals);
+void gen_for_end(char* label, int id);
 void gen_JUMP(char* destination);
-
-
+void gen_JUMPIFEQ(char* destination, char* s1, char *s2);
+void gen_JUMPIFNEQ(char* destination, char* s1, char *s2);
+void gen_stack_instructions(stack_instruction instruction);
 void gen_pushs(char* s1);
 void gen_pops(char* s1);
 void gen_clears();
@@ -52,27 +67,14 @@ void gen_FLOAT2INT(char* s1, char* s2);
 void gen_INT2CHAR(char* s1, char* s2);
 void gen_STRI2INT(char* s1, char* s2);
 void gen_READ(char* s1, char* s2);
-
 void gen_CONCAT(char* s1, char* s2, char* s3);
 void gen_STRLEN(char* s1, char* s2);
 void gen_GETCHAR(char* s1, char* s2, char* s3);
 void gen_SETCHAR(char* s1, char* s2, char* s3);
 void gen_TYPE(char* s1, char* s2);
-
-
-
-
-
-
 void gen_EXIT(char* s1);
 void gen_BREAK();
 void gen_DPRINT(char* s1);
-
-
-
-
 void gen_EOL();
-
-
 
 #endif //GENERATOR_H
